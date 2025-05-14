0cc1bdba325d3f38796e1efb034d0787e80e2fa1
AnaLucila <luly_val@yahoo.com.ar>
2025-05-14 12:22:26 -0300

    cambios 14-5

--- enviar_consulta.php
+++ enviar_consulta.php
@@ -1,14 +1,14 @@
 <?php
-$nombre_form = $_POST["nombre"];
-$apellido_form = $_POST["apellido"];
-$email_form = $_POST["email"];
-$mensaje_form = $_POST["mensaje"];
+$nombre_form = $_POST['nombre'];
+$apellido_form = $_POST['apellido'];
+$email_form = $_POST['email'];
+$mensaje_form = $_POST['mensaje'];
 
 /*$conexion = mysqli_connect ("localhost","root","","curso_wordpress_y_php") or exit ("No se pudo conectar a la base de datos");*/
 
-$conexion = mysqli_connect ("	sql101.infinityfree.com","	if0_38668503","RWmRMFE51sOBYq","if0_38668503_worpress_php") or exit ("No se pudo conectar a la base de datos");
+$conexion = mysqli_connect ('sql101.infinityfree.com','	if0_38668503','RWmRMFE51sOBYq','if0_38668503_worpress_php') or exit ('No se pudo conectar a la base de datos');
 
-mysqli_query ($conexion , "INSERT INTO clientes VALUES (DEFAULT, $nombre_form ,$apellido_form ,$email_form, $mensaje_form)");
+mysqli_query ($conexion , "INSERT INTO clientes VALUES (DEFAULT, '$nombre_form' ,'$apellido_form' ,'$email_form', '$mensaje_form')");
 
 mysqli_close($conexion);
 
