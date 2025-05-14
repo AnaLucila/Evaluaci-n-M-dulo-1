bad25f085a7afd787a27175d0cccf535b1893927
AnaLucila <luly_val@yahoo.com.ar>
2025-05-14 12:23:52 -0300

    14-5

--- enviar_consulta.php
+++ enviar_consulta.php
@@ -6,7 +6,7 @@ $mensaje_form = $_POST['mensaje'];
 
 /*$conexion = mysqli_connect ("localhost","root","","curso_wordpress_y_php") or exit ("No se pudo conectar a la base de datos");*/
 
-$conexion = mysqli_connect ('sql101.infinityfree.com','	if0_38668503','RWmRMFE51sOBYq','if0_38668503_worpress_php') or exit ('No se pudo conectar a la base de datos');
+$conexion = mysqli_connect ('sql101.infinityfree.com','if0_38668503','RWmRMFE51sOBYq','if0_38668503_worpress_php') or exit ('No se pudo conectar a la base de datos');
 
 mysqli_query ($conexion , "INSERT INTO clientes VALUES (DEFAULT, '$nombre_form' ,'$apellido_form' ,'$email_form', '$mensaje_form')");
 
